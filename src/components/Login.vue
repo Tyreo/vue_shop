435e82ef52c8c0e7b438fe9d74c51afe8cd42e35
lijun <jun9.li@lvyuetravel.com>
2021-04-07 23:38:24 +0800

    vue

---
+++ src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="login-container">
+        <div class="login-box">
+            <div class="avatar-box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  setup () {
+
+  }
+}
+</script>
+<style lang="less" scoped>
+.login-container {
+    height: 100%;
+    background-color: #2b4b6b;
+}
+.login-box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+.avatar-box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+        height: 100%;
+        width: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+    }
+}
+</style>
