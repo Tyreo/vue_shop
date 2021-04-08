77d272c6013d372df3cd45d0edfc259adcdd8c7c
lijun <jun9.li@lvyuetravel.com>
2021-04-08 22:47:46 +0800

    login

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,6 @@
 <template>
-    <div class="login-container">
-        <div class="login-box">
-            <div class="avatar-box">
-                <img src="../assets/logo.png" alt="">
-            </div>
-        <el-form ref="loginFormRef" :model="loginForm" :rules="loginRules" label-width="0px" class="form-box">
-            <el-form-item prop="username">
-                <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input v-model="loginForm.password" type="password" prefix-icon="iconfont icon-3702mima"></el-input>
-            </el-form-item>
-            <el-form-item class="btns">
-                <el-button type="primary" @click="login">登陆</el-button>
-                <el-button type="info" @click="resetLoginForm">重置</el-button>
-            </el-form-item>
-        </el-form>
-        </div>
+    <div class="home-container">
+        <el-button type="info" @click="loginOut">退出</el-button>
     </div>
 </template>
 <script>
@@ -58,23 +42,9 @@ export default {
     }
   },
   methods: {
-    resetLoginForm () {
-      this.$refs.loginFormRef.resetFields()
-    },
-    login () {
-      this.$refs.loginFormRef.validate(async (valid) => {
-        if (valid) {
-          const res = await this.$http.post('login', this.loginForm)
-          console.log('login', res)
-          if (res.meta && res.meta.status === 200) {
-            this.$message.success('登录成功')
-            sessionStorage.setItem('TOKEN', res.data.token)
-            this.$router.push('/home')
-          } else {
-            this.$message.error(`登录失败！${res.meta.msg}`)
-          }
-        }
-      })
+    loginOut () {
+      sessionStorage.clear('TOKEN')
+      this.$router.push('/login')
     }
   }
 }
