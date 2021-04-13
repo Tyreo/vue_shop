2d7c1e99f5f14192600ea0e9ca368e6cf2e16269
jun9.li <jun9.li@lvyuetravel.com>
2021-04-13 10:56:11 +0800

    formater

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,47 +1,59 @@
 <template>
-    <el-container class="home-container">
-      <el-header class="home-header">
-        <div class="header-title">
-          <img class="header-icon" src="../assets/heima.png" alt="tupian">
-          <span class="header-name">电商后台管理系统</span>
-        </div>
-        <el-button type="info" @click="loginOut" class="header-button">退出</el-button>
-      </el-header>
-      <el-container>
-        <el-aside :width="isClllapse ? '64px' : '200px'">
-          <div class="toggle-button" @click="toggleCollapse">|||</div>
-          <el-menu
-            background-color="#333744"
-            text-color="#fff"
-            unique-opened
-            router
-            :default-active="activePath"
-            :collapse="isClllapse"
-            :collapse-transition="false"
-            active-text-color="#409eff">
-            <el-submenu v-for="(item, index) in menuLists" :key="index" :index="index + ''">
+  <el-container class="home-container">
+    <el-header class="home-header">
+      <div class="header-title">
+        <img class="header-icon" src="../assets/heima.png" alt="tupian" />
+        <span class="header-name">电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="loginOut" class="header-button"
+        >退出</el-button
+      >
+    </el-header>
+    <el-container>
+      <el-aside :width="isClllapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          unique-opened
+          router
+          :default-active="activePath"
+          :collapse="isClllapse"
+          :collapse-transition="false"
+          active-text-color="#409eff"
+        >
+          <el-submenu
+            v-for="(item, index) in menuLists"
+            :key="index"
+            :index="index + ''"
+          >
+            <template slot="title">
+              <i :class="iconObj[item.id]"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <el-menu-item
+              @click="saveNavState('/' + subItem.path)"
+              v-for="(subItem, index) in item.children"
+              :key="index"
+              :index="'/' + subItem.path"
+            >
               <template slot="title">
-                <i :class="iconObj[item.id]"></i>
-                <span>{{item.authName}}</span>
+                <i class="el-icon-menu"></i>
+                <span>{{ subItem.authName }}</span>
               </template>
-                <el-menu-item @click="saveNavState('/' + subItem.path)" v-for="(subItem, index) in item.children" :key="index" :index="'/' + subItem.path">
-                  <template slot="title">
-                    <i class="el-icon-menu"></i>
-                    <span>{{subItem.authName}}</span>
-                  </template>
-              </el-menu-item>
-            </el-submenu>
-          </el-menu>
-        </el-aside>
-        <el-main>
-          <router-view></router-view>
-        </el-main>
-      </el-container>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
+  </el-container>
 </template>
 <script>
 export default {
-  data () {
+  data() {
     return {
       res: {
         data: [
@@ -159,28 +171,30 @@ export default {
       activePath: ''
     }
   },
-  created () {
+  created() {
     this.getMenuList()
     this.activePath = sessionStorage.getItem('activePath')
   },
   methods: {
-    loginOut () {
+    loginOut() {
       sessionStorage.clear('TOKEN')
       this.$router.push('/login')
     },
-    async getMenuList () {
+    async getMenuList() {
       const res = await this.$http.get('menus')
       if (res && res.meta && res.meta.status === 200) {
         this.menuLists = res.data
       } else {
-        this.$message.error((res && res.meta && res.meta.msg) || '服务器异常，请稍后再试')
+        this.$message.error(
+          (res && res.meta && res.meta.msg) || '服务器异常，请稍后再试'
+        )
       }
       console.log('res', res)
     },
-    toggleCollapse () {
+    toggleCollapse() {
       this.isClllapse = !this.isClllapse
     },
-    saveNavState (avtive) {
+    saveNavState(avtive) {
       sessionStorage.setItem('activePath', avtive)
       this.activePath = avtive
     }
@@ -188,44 +202,44 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-  .el-header {
-    background-color: #373d41;
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 20px;
+  color: #fff;
+  > div {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    font-size: 20px;
-    color: #fff;
-    > div {
-      display: flex;
-      align-items: center;
-      > span {
-        margin-left: 10px;
-      }
+    > span {
+      margin-left: 10px;
     }
   }
-  .el-aside {
-    background-color: #333744;
-    .el-menu {
-      border-right: 0;
-    }
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: 0;
   }
-  .el-main {
-    background-color: #eaedf1;
-  }
-  .home-container {
-    width: 100%;
-    height: 100%;
-  }
-  .iconfont {
-    margin-right: 10px;
-  }
-  .toggle-button {
-    color: #fff;
-    font-size: 10px;
-    line-height: 24px;
-    letter-spacing: 0.2em;
-    text-align: center;
-    background-color: #4a5064;
-    cursor: pointer;
-  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.home-container {
+  width: 100%;
+  height: 100%;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button {
+  color: #fff;
+  font-size: 10px;
+  line-height: 24px;
+  letter-spacing: 0.2em;
+  text-align: center;
+  background-color: #4a5064;
+  cursor: pointer;
+}
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,27 +1,40 @@
 <template>
-    <div class="login-container">
-        <div class="login-box">
-            <div class="avatar-box">
-                <img src="../assets/logo.png" alt="">
-            </div>
-        <el-form ref="loginFormRef" :model="loginForm" :rules="loginRules" label-width="0px" class="form-box">
-            <el-form-item prop="username">
-                <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input v-model="loginForm.password" type="password" prefix-icon="iconfont icon-3702mima"></el-input>
-            </el-form-item>
-            <el-form-item class="btns">
-                <el-button type="primary" @click="login">登陆</el-button>
-                <el-button type="info" @click="resetLoginForm">重置</el-button>
-            </el-form-item>
-        </el-form>
-        </div>
+  <div class="login-container">
+    <div class="login-box">
+      <div class="avatar-box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginRules"
+        label-width="0px"
+        class="form-box"
+      >
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.password"
+            type="password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登陆</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 <script>
 export default {
-  data () {
+  data() {
     return {
       loginForm: {
         username: 'admin',
@@ -58,10 +71,10 @@ export default {
     }
   },
   methods: {
-    resetLoginForm () {
+    resetLoginForm() {
       this.$refs.loginFormRef.resetFields()
     },
-    login () {
+    login() {
       this.$refs.loginFormRef.validate(async (valid) => {
         if (valid) {
           const res = await this.$http.post('login', this.loginForm)
@@ -80,48 +93,48 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-    .login-container {
-        height: 100%;
-        background-color: #2b4b6b;
-    }
-    .login-box {
-        width: 450px;
-        height: 300px;
-        background-color: #fff;
-        border-radius: 3px;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
-    .avatar-box {
-        height: 130px;
-        width: 130px;
-        border: 1px solid #eee;
-        border-radius: 50%;
-        padding: 10px;
-        box-shadow: 0 0 10px #ddd;
-        position: absolute;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background-color: #fff;
-        img {
-            height: 100%;
-            width: 100%;
-            border-radius: 50%;
-            background-color: #eee;
-        }
-    }
-    .form-box {
-        position: absolute;
-        left: 0;
-        top: 100px;
-        padding: 20px;
-        box-sizing: border-box;
-        width: 100%;
-    }
-    .btns {
-        display: flex;
-        justify-content: flex-end;
-    }
+.login-container {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+.login-box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.avatar-box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  img {
+    height: 100%;
+    width: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+.form-box {
+  position: absolute;
+  left: 0;
+  top: 100px;
+  padding: 20px;
+  box-sizing: border-box;
+  width: 100%;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
